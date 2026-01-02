cd0de1be0851ee9f9c7cffb70880f7f33b1c7aeb
Julia Signell <jsignell@gmail.com>
2026-01-02 13:37:36 -0500

    More output

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -13,7 +13,7 @@ mamba list
 # If pytest is not already installed in the image, install it.
 which py.test > /dev/null || \
     echo "Installing pytest inside the image..." && \
-    python3 -m pip install --no-cache "pytest<9" > /dev/null;
+    python3 -m pip install --no-cache "pytest<9";
 
 # If there are any .ipynb files in image-tests, install pytest-notebook
 # if necessary, and set PYTEST_FLAGS so notebook tests are run.
@@ -21,7 +21,7 @@ ls /srv/repo/image-tests/*.ipynb > /dev/null && \
     echo "Found notebooks, using pytest-notebook to run them..." && \
     export PYTEST_FLAGS="--nb-test-files ${PYTEST_FLAGS}" && \
     python3 -c "import pytest_notebook" 2> /dev/null || \
-        python3 -m pip install --no-cache pytest-notebook > /dev/null;
+        python3 -m pip install --no-cache pytest-notebook;
 
 # If regeneration flag is set, add it to the pytest flags
 [ -n "${REGENERATE_OUTPUTS}" ] && \
