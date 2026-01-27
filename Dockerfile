7af523a72801d4f6c6e4f675896a43013f119ecf
Julia Signell <jsignell@gmail.com>
2026-01-27 14:22:02 -0500

    Upgrade to pangeo-notebook 2025.12.30 (#54)
    * Upgrade pangeo notebook image to 2025.12.30
    * Remove pins that get around fixed bugs
    * Remove icechunk and virtualizarr now that they are available upstream
    * Remove stac_ipyleaflet
    * move conda list to Dockerfile
    * Use forked version of repo2docker-action to pin pytest to less than 9
    * Pin pyviz_comms==3.0.4

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM pangeo/pangeo-notebook:2025.08.14
+FROM pangeo/pangeo-notebook:2025.12.30
 
 LABEL org.opencontainers.image.source="https://github.com/nasa-impact/pangeo-notebook-veda-image"
 
@@ -7,6 +7,7 @@ USER ${NB_USER}
 ADD environment.yml environment.yml
 
 RUN conda env update --prefix /srv/conda/envs/notebook --file environment.yml
+RUN conda list
 
 COPY --chown=${NB_USER}:${NB_USER} image-tests /srv/repo/image-tests
 COPY --chown=${NB_USER}:${NB_USER} scripts /srv/repo/scripts
--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,9 +1,5 @@
 export PYTEST_FLAGS="";
 
-# first inspect what all is in the image
-echo "Output of mamba list in the image..."
-mamba list
-
 # If there is a requirements.txt file inside image-tests, install it.
 # Useful if you want to install a bunch of pytest packages.
 [ -f /srv/repo/image-tests/requirements.txt ] && \
@@ -13,7 +9,7 @@ mamba list
 # If pytest is not already installed in the image, install it.
 which py.test > /dev/null || \
     echo "Installing pytest inside the image..." && \
-    python3 -m pip install --no-cache pytest > /dev/null;
+    python3 -m pip install --no-cache "pytest<9" > /dev/null;
 
 # If there are any .ipynb files in image-tests, install pytest-notebook
 # if necessary, and set PYTEST_FLAGS so notebook tests are run.
@@ -21,7 +17,7 @@ ls /srv/repo/image-tests/*.ipynb > /dev/null && \
     echo "Found notebooks, using pytest-notebook to run them..." && \
     export PYTEST_FLAGS="--nb-test-files ${PYTEST_FLAGS}" && \
     python3 -c "import pytest_notebook" 2> /dev/null || \
-        python3 -m pip install --no-cache pytest-notebook > /dev/null;
+        python3 -m pip install --no-cache pytest-notebook;
 
 # If regeneration flag is set, add it to the pytest flags
 [ -n "${REGENERATE_OUTPUTS}" ] && \
